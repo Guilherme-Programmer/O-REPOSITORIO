54730b2591a394e760b976ce8555c32f2a65501e
Guilherme-Programmer <guigoromont@gmail.com>
2026-08-04 19:36:59 -0300

    Update main.py

--- MODULO3/ReciclagemIA/main.py
+++ MODULO3/ReciclagemIA/main.py
@@ -1,34 +1,49 @@
 respostas = {
     "plástico": {
-        "categoria": "Plástico",
+        "categoria": "plástico/embalagens",
         "reciclavel": "Sim",
         "destino": "Coleta seletiva da sua cidade",
         "dica": "Esvazie a garrafa antes do descarte."
     },
 
     "aluminio": {
-        "categoria": "Metal",
+        "categoria": "metal/latas",
         "reciclavel": "Sim",
         "destino": "Coleta seletiva da sua cidade",
-        "dica": "Esvazie o conteúdo antes do descarte."
+        "dica": "Feche/Lacre bem antes do descarte."
     },
       
     "vidro": {
-        "categoria": "Vidro",
+        "categoria": "vidro/garrafas",
         "reciclavel": "Sim",
         "destino": "Coleta seletiva da sua cidade",
-        "dica": "Esvazie o conteúdo antes do descarte."
+        "dica": "Cuide bem antes de fazer o descarte. Pode acabar ouvendo acidentes."
+    },
+    
+    "papel": {
+        "categoria": "papel/papelão",
+        "reciclável": "Não",
+        "destino": "Coleta seletiva da sua cidade",
+        "dica": "Separe o lixo não reciclável do que é reciclável!"
+    },
+    
+    "resíduos animais": {
+        "categoria": "resíduos/fezes",
+        "reciclável": "não",
+        "destino": "Coleta seletiva da sua cidade",
+        "dica": "Embale bem as fezes/resíduos antes de colocar no lixo."
+        
+    } 
+        
     }
-    
-    
-}
 
 while True:
         
         print("♻️ IA DE RECICLAGEM")
+        print("Seja bem vindo!")
         print("--------------------")
 
-        material = input("O que você gostaria de descartar? ").lower()
+        material = input("O que você gostaria de reciclar?").lower()
 
         if material in respostas:
             informacao = respostas[material]
@@ -36,13 +51,14 @@ while True:
             print()
             print("Material:", material)
             print("Categoria:", informacao["categoria"])
-            print("Reciclável:", informacao["reciclavel"])
+            print("Reciclável:", informacao["reciclável"])
             print("Destino:", informacao["destino"])
             print("Dica:", informacao["dica"])
-
+        
+            
         else:
             print()
-            print("Ainda não conheço esse material.")
+            print("Ainda não conheço esse material...")
 
         if material == "nada":
             print("Beleza então, tenha um ótimo dia!")
